227faeea2f7de3529b8baf3501e29d4e0ef4350c
VadimZP <vadymchernykh@gmail.com>
2023-12-28 22:49:57 +0200

    feat: setup react-hook-form and connect it to sign in form

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,11 +1,10 @@
 "use client";
 
 import * as React from "react";
+
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -13,24 +12,32 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { useRouter } from "next/navigation";
+import { CircularProgress } from "@mui/material";
+
 import { useRequestSignIn } from "@/hooks/reactQuery";
+
+import { useForm, Controller } from "react-hook-form";
 
 export default function SignIn() {
   const router = useRouter();
 
   const signInRequest = useRequestSignIn();
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const {
+    control,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-    const formData = new FormData(event.currentTarget);
-
-    const email: string | null = formData.get("email") as string;
-    const password: string | null = formData.get("password") as string;
-
-    if (email !== null && password !== null) {
+  const onSubmit = (data: any) => {
+    if (isValid) {
       signInRequest.mutate(
-        { email, password },
+        { email: data.email, password: data.password },
         {
           onSuccess: () => {
             router.replace("/");
@@ -56,38 +63,62 @@ export default function SignIn() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Controller
             name="email"
-            autoComplete="email"
-            autoFocus
+            control={control}
+            rules={{
+              required: true,
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Invalid email format",
+              },
+            }}
+            render={({ field }) => (
+              <TextField
+                margin="normal"
+                fullWidth
+                id="email"
+                label="Email Address"
+                autoComplete="email"
+                autoFocus
+                helperText={errors.email?.message}
+                error={!!errors.email?.message}
+                {...field}
+              />
+            )}
           />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
+          <Controller
             name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <TextField
+                margin="normal"
+                fullWidth
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                helperText={errors.password?.message}
+                error={!!errors.password?.message}
+                {...field}
+              />
+            )}
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
+
           <Button
+            disabled={signInRequest.isPending || !isValid}
             type="submit"
             fullWidth
             variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+            sx={{ mt: 3, mb: 2, height: "40px" }}
           >
-            Sign In
+            {signInRequest.isPending ? (
+              <CircularProgress size={24} />
+            ) : (
+              "Sign In"
+            )}
           </Button>
           <Grid container>
             <Grid item xs>
@@ -101,7 +132,7 @@ export default function SignIn() {
               </Link>
             </Grid>
           </Grid>
-        </Box>
+        </form>
       </Box>
     </Container>
   );
